d0359b3b9b75371020c85e77a8f2e094101d2889
PatoDeMotoca2 <00001098625055sp@al.educacao.sp.gov.br>
2024-11-05 15:12:22 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Dentre os conflitos que ocorreram por conta da disputa ideológica durante a Guerra Fria podemos citar:?',
+    'Revolução Cubana'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Biologia',
+    'O DNA é composto por nucleotídeos, os quais são compostos por três partes distintas. Quais são elas?',
+    'Pentose, base nitrogenada e fosfato.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Bem-Vindo em Inglês?',
+    'Bem-Vindo em ingles é Welcome'
+)
